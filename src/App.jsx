c867978c1167c02ec0ff475d55b053ca962e3b53
stevec-afk <evets25@gmail.com>
2026-07-10 11:14:46 -0300

    setup parent App with click handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,65 @@
+import Header from "./components/Header.jsx";
+import GameContainer from "./components/GameContainer.jsx";
+import { useState, useEffect } from "react";
+
+const fetchedBeavers = ["Bob", "Trevor", "Kevin"];
+
 function App() {
-  return <></>;
+  const [beavers, setBeavers] = useState(fetchedBeavers);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [clickedBeavers, setClickedBeavers] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
+
+  useEffect(() => {
+    // API call to load beaver data goes here
+  }, []);
+
+  function handleCardClick(cardId) {
+    if (clickedBeavers.includes(cardId)) {
+      if (currentScore > highScore) setHighScore(currentScore);
+      setIsGameOver(true);
+    } else {
+      setClickedBeavers([...clickedBeavers, cardId]);
+
+      const newScore = currentScore + 1;
+      setCurrentScore(newScore);
+      if (newScore > highScore) setHighScore(newScore);
+
+      // Fisher-yates Shuffle logic to shuffle the beavers
+      const shuffledBeavers = [...beavers];
+
+      // Loop backwards through the array
+      for (let i = shuffledBeavers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        // Swap beavers at indices i and j
+        const temp = shuffledBeavers[i];
+        shuffledBeavers[i] = shuffledBeavers[j];
+        shuffledBeavers[j] = temp;
+      }
+      setBeavers(shuffledBeavers);
+    }
+  }
+
+  function resetGame() {
+    setCurrentScore(0);
+    setClickedBeavers([]);
+    setIsGameOver(false);
+  }
+
+  return (
+    <div className="app-container-wrapper">
+      <Header currentScore={currentScore} highScore={highScore} />
+      <GameContainer
+        beavers={beavers}
+        clickedBeavers={clickedBeavers}
+        onCardClick={handleCardClick}
+        isGameOver={isGameOver}
+        onReset={resetGame}
+      />
+    </div>
+  );
 }
 
 export default App;
